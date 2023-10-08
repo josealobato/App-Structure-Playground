d994a571db2a362df2aed82272e9fbc95d515047
Jose A Lobato <21879+josealobato@users.noreply.github.com>
2023-10-08 11:39:04 +0200

    details on the repository protocol

--- feature-adapter-worker-repository/BaseApp/BaseApp/Packages/Repositories/Sources/Repositories/Users/UsersRepositoryProtocol.swift
+++ feature-adapter-worker-repository/BaseApp/BaseApp/Packages/Repositories/Sources/Repositories/Users/UsersRepositoryProtocol.swift
@@ -5,12 +5,17 @@ import Foundation
 
 public protocol UsersRepositoryProtocol {
    
-    // Strategy 1: Arrays: remote and cache when not available.
+    // Strategy 1: Arrays: Returns the remote data and local data only when remote is not available.
     
     func usersFirstPage() async throws -> [UserDataEntity]
     func usersNextPage() async throws -> [UserDataEntity]
     
-    // Strategy 2: Streams: local, the remote and update local with remote.
+    // Strategy 2: Streams: returns local, then the remote and finally update local with
+    // the new remote data.
+    // This strategy is not currently used in the application since it is under development.
+    // But, it is implemented and you can see all happy path unit test in files
+    // `UserRepositoryStreamStrategyFirstPageTests` and
+    // `UserRepositoryStreamStrategyNextPageTests`
     
     func usersFirstPage() -> AsyncStream<[UserDataEntity]>
     func usersNextPage() -> AsyncStream<[UserDataEntity]>
